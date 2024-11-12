dd51703a8ce09e1effee15c78c3970235dff7d02
Achille Bergeret <achillebergeret31@gmail.com>
2024-11-12 22:32:54 +0100

    add the file with the pdf reader library

---
+++ pypdf_test.py
@@ -0,0 +1,9 @@
+from pypdf import PdfReader
+from tkinter import filedialog as fd
+import tkinter as tk
+filename = fd.askopenfilename()
+
+reader = PdfReader(filename)
+number_of_pages = len(reader.pages)
+page = reader.pages[0]
+text = page.extra
